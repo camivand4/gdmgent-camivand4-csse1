06f1df1902a23c15fa4dc91025386a5185fec938
pgmgent-camivand4 <camivand4@student.arteveldehs.be>
2019-10-10 14:57:36 +0200

    opdracht3

--- lessen/les1/Program.cs
+++ lessen/les1/Program.cs
@@ -6,7 +6,39 @@ namespace les1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int getal;
+            getal = 6;
+
+            int tweedeGetal;
+            tweedeGetal = 5;
+
+            if(getal<=tweedeGetal){
+                Console.WriteLine("de waarde van het getal is: " + getal + " kleiner dan of gelijk dan " + tweedeGetal);
+                getal++;
+            } else {
+                Console.WriteLine("de waarde van het getal is: " + getal + " groter dan " + tweedeGetal);
+                getal++;
+            }
+
+            // Switch case
+
+
+            int caseSwitch = 0;
+      
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    caseSwitch++;
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    caseSwitch++;
+                    break;
+                default:
+                    Console.WriteLine("Default case, het nummer is: " + caseSwitch);
+                    break;
+            }
         }
     }
 }
---
+++ lessen/les3/Tafels/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tafels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef je getal in:");
+            int getal1;
+            getal1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geef het 2de getal in:");
+            int getal2;
+            getal2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < getal2+1; i++)
+            {
+                Console.WriteLine(getal1 + " * " + i + " = " + getal1*i);
+            }
+
+        }
+    }
+}
