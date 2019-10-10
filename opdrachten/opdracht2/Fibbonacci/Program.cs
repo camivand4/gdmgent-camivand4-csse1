b01691a7e51f9ced64b1ea5618e2c6fc9d8b455f
pgmgent-camivand4 <camivand4@student.arteveldehs.be>
2019-10-10 14:23:21 +0200

    oefeningen

---
+++ opdrachten/opdracht2/Fibbonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibbonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef het aantal keer in:");
+            int aantalKeer;
+            aantalKeer = Convert.ToInt32(Console.ReadLine());
+            // int aantalKeer = 5;
+            int getal1 = 0;
+            int getal2 = 1;
+            int getal3;
+
+                Console.WriteLine('\n');
+            for (int i = 0; i < aantalKeer; i++)
+            {
+                getal3 = getal1 + getal2;
+                Console.WriteLine(getal3+" ");
+                getal1 = getal2;
+                getal2 = getal3;
+            }
+
+
+        }
+    }
+}
