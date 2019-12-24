9c42d555249d04fb21fd8e312b1037de37009a2d
pgmgent-camivand4 <camivand4@student.arteveldehs.be>
2019-12-24 14:53:57 +0100

    opdracht 8

---
+++ opdrachten/opdracht8/les8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace les8
+{
+    public abstract class Winkelkar
+    {
+        public abstract void Ontgrendelen();
+        public abstract void Rijden();
+    }
+
+        // dit schrijven en dan op lampje
+        // public class PlasticKar : Winkelkar {
+        // public PlasticKar() {
+            
+        // }
+        // }
+
+    public class PlasticKar : Winkelkar {
+        public PlasticKar() {
+            
+        }
+
+		public override void Ontgrendelen()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Rijden()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
